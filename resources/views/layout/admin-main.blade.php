ed863cf3e299599ed50519c6c145d94db0fea9be
LailaWulandarii <E41220418@student.polije.ac.id>
2024-01-25 12:01:03 +0700

    add color palette

--- resources/views/layout/admin-main.blade.php
+++ resources/views/layout/admin-main.blade.php
@@ -48,7 +48,7 @@
             Nav header start
         ***********************************-->
         <div class="nav-header">
-            {{-- <div class="brand-logo">
+            <div class="brand-logo">
                 <a href="index.html">
                     <b class="logo-abbr"><img src="images/logo.png" alt=""> </b>
                     <span class="logo-compact"><img src="./images/logo-compact.png" alt=""></span>
@@ -56,7 +56,7 @@
                         <img src="images/logo-text.png" alt="">
                     </span>
                 </a>
-            </div> --}}
+            </div>
         </div>
         <!--**********************************
             Nav header end
